f21f4c68bf25d20fd70dfb70760f31d38c1c0d90
lestari15 <lestari.18411045@student.ubl.ac.id>
2020-10-13 09:00:46 -0700

    first commit

---
+++ app/src/main/java/com/mebel/tokomebell/users/loginActivity.java
@@ -0,0 +1,34 @@
+package com.mebel.tokomebell.users;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.mebel.tokomebell.R;
+
+public class loginActivity extends AppCompatActivity {
+
+    Button btnLogin;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        getSupportActionBar().hide();
+
+        btnLogin = (Button) findViewById(R.id.btnLogin);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(loginActivity.this, RegistrasiActivity.class);
+                startActivity(i);
+                finish();
+            }
+        });
+    }
+}
